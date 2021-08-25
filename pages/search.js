1e4de8078f2f9a3891b04691136a146980087320
Patrick <patrick.suehrer@thedac.info>
2021-08-25 08:46:47 +0200

    added search functionality

---
+++ pages/search.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+function Search() {
+  return (
+    <div>
+      <Head>
+        <title>Search Results</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
+
+export default Search
